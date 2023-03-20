716edaaa8a5b05ea9ec59dde261d89f3ea0f70b5
ИИП ТТ <kab31lab@gmail.com>
2023-03-20 09:00:24 +0200

    задача 1

--- Program.cs
+++ Program.cs
@@ -10,6 +10,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //Определить общую массу груза.
+            int[] weights = { 10, 20, 30, 40, 50 };
+            int n = weights.Length;
+            int totalWeight = 0;
+            for (int i = 0; i < n; i++)
+            {
+                totalWeight += weights[i];
+            }
+            Console.WriteLine("Общая масса груза: " + totalWeight);
+            Console.Read();
         }
     }
 }
